81156c130a7712154aece42653cc36f5ba3a105a
Nguyen Le Anh Vu <vunguyen.0208200402@gmail.com>
2025-05-01 10:58:41 +0700

    resolve conflict

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middleware
+app.use(morgan("dev")); // 5 types: dev, combined, short, common, tiny
+app.use(helmet());
+app.use(compression());
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const helloStr = "Hello!";
+  return res.status(200).json({
+    message: "Welcome!",
+    metadata: helloStr.repeat(200000),
+  });
+});
+// handle errors
+
+module.exports = app;
